a53f8a94393c4bb7a382b7a85ae4594b79f2dba0
Erfan <mrenmityerf@gmail.com>
2026-02-09 12:17:43 +0330

    chore: remove unused test files for invidious instances
    The test files were used for development but are no longer needed in the codebase. Removing them reduces clutter and avoids confusion.

--- test_instances.js
+++
@@ -1,5 +0,0 @@
-// Test script to check if dynamic instances are loaded
-import { DYNAMIC_INVIDIOUS_INSTANCES } from './components/core/api';
-
-console.log('Dynamic instances:', DYNAMIC_INVIDIOUS_INSTANCES);
-console.log('Number of instances:', DYNAMIC_INVIDIOUS_INSTANCES.length);
